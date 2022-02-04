e3cac21efc3f85e5579acbe54c1c03542b8614bd
ryota-1231 <rrrrr.1231@gmail.com>
2022-02-05 01:23:59 +0900

    料理ジャンル検索実装に伴う変更

--- src/components/pages/top/presenter.tsx
+++ src/components/pages/top/presenter.tsx
@@ -1,46 +1,74 @@
-import Header from './header';
-import MainContainer from './mainContainer';
+import { useContext } from 'react';
 
-const Presenter = () => (
-  <div>
-    <Header />
-    <MainContainer />
-    {/* mainWrapper */}
-    {/* main */}
-    {/* areaContents */}
-    {/* genreContents */}
-    {/* Special feature */}
-    {/* scene */}
-    {/* Commitment conditions */}
-    {/* Featured gourmet articles */}
-    {/* word of mouth */}
-    {/* main */}
-    {/* sidebar */}
-    {/* counter */}
-    {/* myself info */}
-    {/* history */}
-    {/* information */}
-    {/* limited plan */}
-    {/* pr */}
-    {/* contents */}
-    {/* sidebar */}
-    {/* mainWrapper */}
-    {/* bottomWrapper */}
-    {/* tabelog award */}
-    {/* hyaku meiten */}
-    {/* search */}
-    {/* top */}
-    {/* user search */}
-    {/* top */}
-    {/* bottom */}
-    {/* genre ranging search */}
-    {/* others */}
-    {/* bottom */}
-    {/* search */}
-    {/* bottom contents */}
-    {/* footer */}
-    {/* bottomWrapper */}
-  </div>
-);
+import { PrefectureContext } from '../../../contexts/prefectureContext';
+
+import AreaSearch from './areaSearch';
+import AreaSearchDetail from './areaSearchDetail';
+import CookingGenre from './cookingGenre';
+import Header from './header';
+import SidebarColumn from './sidebarColumn';
+import {
+  StyledAreaSearchContainer,
+  StyledMainColumn,
+  StyledMainContainer,
+  StyledMainContents,
+} from './style';
+
+const Presenter = () => {
+  const prefectureContext = useContext(PrefectureContext);
+  return (
+    <div>
+      <Header />
+      <StyledMainContainer>
+        <StyledMainContents>
+          <StyledMainColumn>
+            <StyledAreaSearchContainer>
+              <AreaSearch />
+              {!prefectureContext.isRemove ? <AreaSearchDetail /> : ''}
+            </StyledAreaSearchContainer>
+            <CookingGenre />
+          </StyledMainColumn>
+          <SidebarColumn />
+        </StyledMainContents>
+      </StyledMainContainer>
+
+      {/* mainWrapper */}
+      {/* main */}
+      {/* areaContents */}
+      {/* genreContents */}
+      {/* Special feature */}
+      {/* scene */}
+      {/* Commitment conditions */}
+      {/* Featured gourmet articles */}
+      {/* word of mouth */}
+      {/* main */}
+      {/* sidebar */}
+      {/* counter */}
+      {/* myself info */}
+      {/* history */}
+      {/* information */}
+      {/* limited plan */}
+      {/* pr */}
+      {/* contents */}
+      {/* sidebar */}
+      {/* mainWrapper */}
+      {/* bottomWrapper */}
+      {/* tabelog award */}
+      {/* hyaku meiten */}
+      {/* search */}
+      {/* top */}
+      {/* user search */}
+      {/* top */}
+      {/* bottom */}
+      {/* genre ranging search */}
+      {/* others */}
+      {/* bottom */}
+      {/* search */}
+      {/* bottom contents */}
+      {/* footer */}
+      {/* bottomWrapper */}
+    </div>
+  );
+};
 
 export default Presenter;
--- src/components/pages/top/style.ts
+++ src/components/pages/top/style.ts
@@ -1,25 +1,29 @@
 import styled from 'styled-components/macro';
 
-import Color from '../../../styles/const/Color';
-import Size from '../../../styles/const/Size';
-
-export const StyledWrapper = styled.div`
-  background-color: ${Color.SECONDLY};
-  height: 100vh;
-`;
-export const StyledMainContents = styled.div`
-  width: 60%;
-  margin: 0 auto;
-  padding-top: 24px;
-`;
-export const StyledContent = styled.div`
-  background-color: #fff;
+export const StyledMainContainer = styled.div`
+  padding-top: 100px;
+  min-width: 1260px;
   width: 100%;
-  margin: 0 auto;
 `;
-export const StyledP = styled.p`
-  font-size: ${Size.FONT.L}px;
-  font-weight: bold;
-  word-spacing: 2rem;
-  text-align: center;
+
+export const StyledMainContents = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 0 70px;
+  min-width: 1260px;
+  max-width: 1540px;
+  justify-content: space-between;
+`;
+
+export const StyledMainColumn = styled.div`
+  width: 735px;
+  min-width: 800px;
+  max-width: 980px;
+`;
+
+export const StyledAreaSearchContainer = styled.div`
+  position: relative;
+  z-index: 2;
+  min-height: 485px;
+  display: flex;
 `;
